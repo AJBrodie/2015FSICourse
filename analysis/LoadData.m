88d7ae897ac3fd2b3bab426c0797b0ef84206c7b
SaibotTe <tobias.c.te@googlemail.com>
2015-08-12 01:33:42 +0200

    New Postprocessing
    Postprocessing for video.

--- analysis/LoadData.m
+++ analysis/LoadData.m
@@ -6,20 +6,20 @@ filepath = strcat(filename);
 
 % Open the file
 fileID = fopen(filepath,'r');
-fgetl(fileID)
-line = fgetl(fileID)
+fgetl(fileID);
+line = fgetl(fileID);
 lengthOfVector = cell2mat(textscan(line,  '%*s %f',1));
 
 numberOfTimeSteps = 1;
 
 while ~feof(fileID)
-    line = fgetl(fileID)
+    line = fgetl(fileID);
     TimeSteps(numberOfTimeSteps) = cell2mat(textscan(line,  '%f',1));
     for i=1:lengthOfVector
-        line = fgetl(fileID)
+        line = fgetl(fileID);
         Pressure(numberOfTimeSteps,i) = cell2mat(textscan(line,  '%f',1));
-        Velocity_X(numberOfTimeSteps,i) = cell2mat(textscan(line,  '%f',1));
-        Velocity_Y(numberOfTimeSteps,i) = cell2mat(textscan(line,  '%f',1));
+        Velocity_X(numberOfTimeSteps,i) = cell2mat(textscan(line,  '%*f %f',1));
+        Velocity_Y(numberOfTimeSteps,i) = cell2mat(textscan(line,  '%*f %*f %f',1));
     end
     numberOfTimeSteps = numberOfTimeSteps + 1;
 end
